7c2fa6afe9f0d81f3b67284b005bfa1a3141b06c
DustinDEV2more <Dustin@Dustin-DM.de>
2020-12-02 19:25:54 +0100

    fresh start

--- models/DAILY COINS.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const schema = mongoose.Schema({
-    user: String,
-    time: {type: Date, default: new Date()}
-})
-
-module.exports = mongoose.model("daily_coin", schema)
