af8d4f10b3962a8d4af19eb540917794f7d6d856
Dustin_DM <private@dustin-dm.de>
2020-12-12 13:11:55 +0100

    add new shop item

---
+++ events/shop/beta-musik.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'Beta Music Command',
+	description: 'Erlaubt es dir, über den neuen Beta Bot den neuen Musik Befehl auszuführen. (Wenn die Beta Phase des Bots vorüber ist wird diese Funktion gratis)',
+    amount: 1500,
+	id: 981,
+	canOnlyBuyedOnce: true,
+	async execute(member, memberdb) {
+       
+	},
+};
