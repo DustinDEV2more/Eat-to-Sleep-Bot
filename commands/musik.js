4f43eee392556d4094aba274e800f3f9965d9b8c
DustinDEV2more <Dustin@Dustin-DM.de>
2020-12-12 15:31:59 +0100

    add Music command

---
+++ commands/musik.js
@@ -0,0 +1,92 @@
+var musicsave = []
+
+module.exports = {
+	name: 'Musik',
+	call: 'm',
+	description: 'Spielt Musik in deinem Voice Channel von YouTube ab',
+    usage: `m [play (youtube-link)/stop/skip/ls (lautstärke count)/queue]`,
+    permissions: [],
+	async execute(message, args) {
+		const embed = require("../Embed")
+		const ytdl = require('ytdl-core');
+		const OLDMEMBER = require("../Models/OLD-MEMBER")
+
+		const mdata = await OLDMEMBER.findOne({"info.id": message.author.id})
+		if (mdata.coins.purchases.find(x => x.id == 981) === undefined) return message.channel.send(embed.error_user("Fehlende Berechtigung", "Du kannst diesen Command aktuell nicht benutzten. Die Rechte für diesen Command lassen sich im Shop erwerben"))
+	
+		//Adding Song to queue
+		if (args[0].toLocaleLowerCase() == "play"){
+			if (!args[1]) return message.channel.send(embed.error_user("Kein Song angegeben", "Du musst einen YouTube Video Link angeben"))
+			
+			//Fetch Song Infoinformation
+			//let info = await ytdl.getBasicInfo(args[1]);
+			var info = await ytdl.getBasicInfo(args[1]);
+			var Videoinfos = {
+				title: info.videoDetails.title,
+				url: info.videoDetails.video_url,
+				author: info.videoDetails.ownerChannelName,
+				author_url: info.videoDetails.ownerProfileUrl,
+				video_lenght: info.videoDetails.lengthSeconds
+			}
+			if (!info.videoDetails.availableCountries.find(x => x == "FR")) return message.channel.send(embed.error_system("Song nicht abspielbar", "Leider kann ich diesen Song nicht abspielen da mein Anti-DDOS Provider in Frankreich liegt und das Video in Frankreich nicht verfügbar ist"))
+
+			//check if bot is already playing a song in that server
+			if (musicsave[message.channel.guild.id]){
+				//already playing
+				var position = musicsave[message.channel.guild.id].queue.push(Videoinfos);
+				
+				return message.channel.send(embed.success("Song hinzugefügt", `Ich habe [${Videoinfos.title}](${Videoinfos.url}) zur queue auf **position ${position}** hinzugefügt`))
+			
+			}
+			else {
+			//nothing playing
+			if (message.member.voice.channel) {
+				musicsave[message.channel.guild.id] = {queue: [Videoinfos]};
+
+				play(message.channel.guild.id)
+				return message.channel.send(embed.success("Song hinzugefügt", `Ich werde [${Videoinfos.title}](${Videoinfos.url}) in deinem Voicechat abspielen`))
+
+				} 
+				  
+				else {
+				message.channel.send(embed.error_user("Kein Voice Channel", "Du musst dich in einem Voicechannel befinden damit ich Musik spielen kann"));
+				}
+			}
+		}
+
+		if (args[0].toLocaleLowerCase() == "stop"){
+			if (!musicsave[message.channel.guild.id]) return message.channel.send(embed.error_user("Keinen Player gefunden", "Mir ist kein Channel bewusst in dem ich gerade einen Song abspiele"))
+			musicsave[message.channel.guild.id].queue = [];
+			musicsave[message.channel.guild.id].dispatcher.end();
+			message.channel.send(embed.success("Musik gestoppt", "Ich habe den aktuellen Song gestoppt und die queue geleert"))
+		}
+
+		if (args[0].toLocaleLowerCase() == "skip"){
+			if (!musicsave[message.channel.guild.id]) return message.channel.send(embed.error_user("Keinen Player gefunden", "Mir ist kein Channel bewusst in dem ich gerade einen Song abspiele"))
+			musicsave[message.channel.guild.id].dispatcher.end();
+			message.channel.send(embed.success("Song übersprungen", "Ich habe den aktuellen Song übersprungen"))
+		}
+
+		function play(GuildId) {
+			if (musicsave[GuildId].queue.length == 0) {
+				//Leave Voicechannel if queue is empty
+				message.channel.guild.members.cache.get("785505459070959636").voice.channel.leave();
+				musicsave[GuildId] = null
+			  } else if (musicsave[GuildId].queue.length > 0){
+				message.member.voice.channel.join().then(d => {
+					musicsave[GuildId].dispatcher = d.play(
+					  ytdl(musicsave[GuildId].queue[0].url, 
+						 { filter: 'audioonly' }
+						)
+					  );
+					
+					  musicsave[GuildId].dispatcher.on("finish", async () => {
+						  console.log("finished")
+						musicsave[GuildId].queue.shift();
+						play(GuildId)
+				  })
+			  })
+			}
+		}
+	},
+};
