b096fbf5871acfac4f62542fdbeff83c600d3fb5
DustinDEV2more <Dustin@Dustin-DM.de>
2020-12-27 23:31:19 +0100

    add cookies

--- webserver/webmain.js
+++ webserver/webmain.js
@@ -2,6 +2,8 @@ const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser');
 var expressLayouts = require('express-ejs-layouts');
+var cookieParser = require('cookie-parser');
+
 
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
@@ -10,6 +12,7 @@ app.use(expressLayouts)
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cookieParser())
 
 const api = require("./routes/api")
 app.use("/api", api)
