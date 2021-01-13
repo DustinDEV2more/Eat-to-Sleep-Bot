aaae9745794dd294bd6a735276aa3039d135492c
DustinDEV2more <Dustin@Dustin-DM.de>
2021-01-13 19:41:15 +0100

    add joining members to database

---
+++ events/welcome.js
@@ -0,0 +1,11 @@
+var {client} = require("../index")
+var MEMBER = require("../Models/MEMBER")
+
+client.on("guildMemberAdd", async (member) => {
+    var memberdb = await MEMBER.findOne({"id": member.id})
+
+    if (!member){
+        await new MEMBER({"id": member.id, informations: {"name": member.user.username, "avatar": member.user.avatar, "discriminator": member.user.discriminator}).save()
+        console.log(`Created new DB index for ${member.user.username}`)
+    }
+})
