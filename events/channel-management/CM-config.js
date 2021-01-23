aff7015886e20cb7301bb130172f429047e75171
Dustin_DM <private@dustin-dm.de>
2021-01-23 19:40:53 +0100

    add Monthly Mario Kart

--- events/channel-management/CM-config.js
+++ events/channel-management/CM-config.js
@@ -1,5 +1,5 @@
 const { RichEmbed } = require('discord.js')
-//     30    18    *    *    1
+//     30    18   10    *    *
 // ┬    ┬    ┬    ┬    ┬    ┬
 // │    │    │    │    │    │
 // │    │    │    │    │    └ day of week (0 - 7) (0 or 7 is Sun)
@@ -11,22 +11,22 @@ const { RichEmbed } = require('discord.js')
 
 var channelMclass = require("../channel-management/ChannelM-class")
 
-// var Monday_Mario_Kart = new channelMclass(
-// "30 18 * * 1", //start time
-// "30 19 * * 1", //end time
-// "Monday Mario Kart", //Event Name
-// "Mario Kart Talk", // Talk Name ">>Talk<< 1", ">>Talk<< 2"
-// "Mario Kart Infos", //Text Channel Name
-// new RichEmbed() //Embed wish will be send in the Text Channel 
-// .setColor("#0984e3")
-// .setThumbnail("https://www.mariowiki.com/images/thumb/7/71/Crazy8MK8.png/1200px-Crazy8MK8.png")
-// .setTitle("Monday Mario Kart").setDescription("Das heutige Monday Mario Kart auf Eat, Sleep, Nintendo, Repeat hat nun begonnen")
-// .addField("Infos:", "Start: 18:30 Uhr\nEnde: 19:30 Uhr\nTurnier Code: 2657-4724-8683")
-// .addField("Eure Teilnahme Geschenke:", "2* XP Boost in MMK Talks"),
-// "585523787408212079", //Copie Permission from this Categorie ID
-// "585523787408212079",  //Send Members back to this Categorie ID
-// true, //xp x 2 > true or false
-// )
+var Monday_Mario_Kart = new channelMclass(
+"30 18 10 * *", //start time
+"30 19 10 * *", //end time
+"Monthly Mario Kart", //Event Name
+"Mario Kart Talk", // Talk Name ">>Talk<< 1", ">>Talk<< 2"
+"Mario Kart Infos", //Text Channel Name
+new RichEmbed() //Embed wish will be send in the Text Channel 
+.setColor("#0984e3")
+.setThumbnail("https://www.mariowiki.com/images/thumb/7/71/Crazy8MK8.png/1200px-Crazy8MK8.png")
+.setTitle("Monday Mario Kart").setDescription("Das heutige Monthly Mario Kart auf Eat, Sleep, Nintendo, Repeat hat nun begonnen")
+.addField("Infos:", "Start: 18:30 Uhr\nEnde: 19:30 Uhr\nTurnier Code: 2657-4724-8683")
+.addField("Eure Teilnahme Geschenke:", "2* XP Boost in MMK Talks"),
+"585523787408212079", //Copie Permission from this Categorie ID
+"585523787408212079",  //Send Members back to this Categorie ID
+true, //xp x 2 > true or false
+)
 
 var Among_Us_Donnerstag = new channelMclass(
 "30 19 * * 4", //start time
