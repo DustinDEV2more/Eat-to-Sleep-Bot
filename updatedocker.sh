74bc275626131e1918fc1b7f8d9e69e33ca6e740
DustinDEV2more <Dustin@Dustin-DM.de>
2021-03-08 22:57:03 +0100

    remove memory controll from docker

--- updatedocker.sh
+++ updatedocker.sh
@@ -1,3 +1,3 @@
 docker build -t dustin/beta-bot . 
 docker rm --force beta-bot
-docker run -d --restart always --cap-add=SYS_ADMIN --name beta-bot --memory=1500m --cpus=0.5 -p 7869:7869 dustin/beta-bot
+docker run -d --restart always --cap-add=SYS_ADMIN --name beta-bot -p 7869:7869 dustin/beta-bot
